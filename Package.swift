a66376b8da26793a0a430fb73e11a670f11c5d64
Gwendal Roué <gr@pierlis.com>
2024-09-29 11:54:12 +0200

    Bump watchOS requirement in Package.swift

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
         .iOS(.v13),
         .macOS(.v10_15),
         .tvOS(.v13),
-        .watchOS(.v6),
+        .watchOS(.v7),
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
